0d21198bd0a673b08a6347683c82e07c3d4f5370
hishira <okami0124@gmail.com>
2020-10-17 00:03:23 +0200

    Add left panel appear when we click left click

--- flexbox/flexwithstartanimation/main.js
+++ flexbox/flexwithstartanimation/main.js
@@ -1,4 +1,4 @@
-
+let leftpanel = false
 function indissapearbuttonclick(event){
     let clickedElement = event.target;
     console.log(clickedElement)
@@ -6,12 +6,23 @@ function indissapearbuttonclick(event){
     setTimeout(()=>{
         clickedElement.classList.add("buttonendanimation-secondanimation")
         setTimeout(()=>{
-            let element = document.getElementsByClassName("entrance")[0]
-            element.classList.add("entrance-animation")
-            console.log(element)
+            let element = document.getElementsByClassName("entrance")[0];
+            element.classList.add("entrance-animation");
+            console.log(element);
             
         },250)
-    },450)
-    
-    
+    },450);
+}
+function leftpanelClickHandle(){
+    if (leftpanel === false){
+        let element = document.getElementsByClassName("leftcontainer")[0];
+        element.classList.remove("leftcontainer-animation-back")
+        element.classList.add("leftcontainer-animation");
+        leftpanel=!leftpanel
+    }else{
+        let element = document.getElementsByClassName("leftcontainer")[0];
+        element.classList.remove("leftcontainer-animation")
+        element.classList.add("leftcontainer-animation-back")
+        leftpanel=!leftpanel
+    }
 }
