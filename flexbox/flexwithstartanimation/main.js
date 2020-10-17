5ac8fb55403388b6a8d49663e3fc23cfb0eb026f
hishira <okami0124@gmail.com>
2020-10-17 02:12:15 +0200

    #3

--- flexbox/flexwithstartanimation/main.js
+++ flexbox/flexwithstartanimation/main.js
@@ -18,11 +18,19 @@ function leftpanelClickHandle(){
         let element = document.getElementsByClassName("leftcontainer")[0];
         element.classList.remove("leftcontainer-animation-back")
         element.classList.add("leftcontainer-animation");
+        let elementnext = document.getElementsByClassName("left-word-visible")[0]
+        elementnext.classList.add("left-word-novisible")
+        let elementnextnext = document.getElementsByClassName("leftclose")[0]
+        elementnextnext.classList.add("leftclose-visible")
         leftpanel=!leftpanel
     }else{
         let element = document.getElementsByClassName("leftcontainer")[0];
         element.classList.remove("leftcontainer-animation")
         element.classList.add("leftcontainer-animation-back")
+        let elementnext = document.getElementsByClassName("left-word-visible")[0]
+        elementnext.classList.remove("left-word-novisible")
+        let elementnextnext = document.getElementsByClassName("leftclose")[0]
+        elementnextnext.classList.remove("leftclose-visible")
         leftpanel=!leftpanel
     }
 }
