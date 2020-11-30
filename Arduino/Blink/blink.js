93e4ef063f4521efded5f41b46b46c8646ab97c0
hishira <okami0124@gmail.com>
2020-11-30 21:16:51 +0100

    In arduino/blink add one more button, which turn on randomly one of two led lamp, yellow or blue, it is easy program but can show we can write program arduino using javascript

--- Arduino/Blink/blink.js
+++ Arduino/Blink/blink.js
@@ -1,22 +1,37 @@
 const { Board, Led, Button } = require("johnny-five");
 const board = new Board({ port: "COM3" });
 let flag = false;
+let sf = false;
 board.on("ready", () => {
   const led = new Led(13);
+  const led2 = new Led(12);
+  const led3 = new Led(11);
   let button = new Button({ pin: 2 });
+  let button3 = new Button({pin:3});
   button.on("down", () => {
         if(!flag)
             led.on();
         else
             led.off();
         flag=!flag
-        console.log("donw");
   });
-  button.on("release", () => {
-    //led.off();
-    console.log("release");
-  });
+  button3.on("down",()=>{
+    if(!sf){
+      let rand = Math.random();
+      if(rand  < 0.5)
+        led2.on();
+      else
+        led3.on();
+      sf=!sf;
+    }else{
+      led2.off();
+      led3.off();
+      sf=!sf;
+    }
+  })
   board.on("exit", () => {
-    //    led.off();
+    led.off();
+    led2.off();
+    led3.off();
   });
 });
