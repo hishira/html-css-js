a30d68f4cd7eaae8a34ae865583d124f7b677bc0
hishira <okami0124@gmail.com>
2023-01-22 22:28:48 +0100

    Refactor main page and create more utils function

--- Portfolio/src/utils/helper.ts
+++ Portfolio/src/utils/helper.ts
@@ -1,3 +1,4 @@
+
 export const isNil = (value: any): boolean =>{
     return value === null || value === undefined;
 }
@@ -5,3 +6,17 @@ export const isNil = (value: any): boolean =>{
 export const isNilOneOfThem = (...value: any[]): boolean =>{
     return value.filter(isNil).length > 0;
 }
+
+export const isUndefined = (value: any): boolean => {
+    return value === undefined;
+    
+}
+export const isOneOfThemUndefined = (...value: any[]): boolean => {
+    return value.filter(isUndefined).length > 0;
+}
+
+export const isNotUndefined = (value: any): boolean => {
+    return value !== undefined;
+}
+
+
